97b55ef9eae4d54357d34f7b1468f7f14f86db19
maxsudik <maxsudik@gmail.com>
2021-12-14 15:36:43 -0800

    Adding lifecycle annotations

--- src/test/java/junit5Test/FirstTest.java
+++ src/test/java/junit5Test/FirstTest.java
@@ -1,9 +1,29 @@
 package junit5Test;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class FirstTest {
+
+    @BeforeAll
+    public void beforeAllMethod() {
+        System.out.println("--This is the before All method");
+    }
+
+    @BeforeEach
+    public void beforeEachMethod() {
+        System.out.println("----This is the before Each method");
+    }
+
+    @AfterAll
+    public void afterAllMethod() {
+        System.out.println("--This is the the after ALl method");
+    }
+
+    @AfterEach
+    public void afterEachMethod() {
+        System.out.println("----This is the after Each method");
+    }
 
     @Test
     public void firstMethod() {
